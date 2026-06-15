aa4379c4d5dac4cbbd954eedbdf0bfa3067a1f62
Jorge Ventura <compartiendo.ideas@gmail.com>
2026-06-15 07:04:09 -0600

    Update Google site verification meta tag

--- src/zona/templates/layout.js
+++ src/zona/templates/layout.js
@@ -18,7 +18,7 @@ function layout({ title, desc, canonical, ogImage, body, scripts = '',
 <html lang="es">
 <head>
 <meta charset="UTF-8">
-<meta name="google-site-verification" content="24850a801f739dec">
+<meta name="google-site-verification" content="O8Y0pUTjSvnMM9c7Mq82yy5u5OoqlYBThE0aLzTDASA">
 <meta name="viewport" content="width=device-width,initial-scale=1">
 <title>${pageTitle}</title>
 <meta name="description" content="${metaDesc}">
