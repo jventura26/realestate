c9d29d5ff693bca09b1d9208c5a2e25433a366fc
Jorge Ventura <compartiendo.ideas@gmail.com>
2026-06-14 22:40:56 -0600

    Add favicon and apple-touch-icon links to layout

--- src/vinculo/templates/layout.js
+++ src/vinculo/templates/layout.js
@@ -23,6 +23,8 @@ fbq('track','PageView');
 <!-- End Meta Pixel Code -->
 
 <meta charset="UTF-8">
+<link rel="icon" type="image/png" href="/assets/favicon.png">
+<link rel="apple-touch-icon" href="/assets/favicon.png">
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,300;0,400;0,500;1,300;1,400&display=swap">
 <meta name="viewport" content="width=device-width,initial-scale=1">
