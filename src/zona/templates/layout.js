66905c35b47a23006e9d49e76dd24d7ba6639cef
Zona INNmueble <compartiendo.ideas@gmail.com>
2026-07-12 21:36:24 -0600

    Update layout.js

--- src/zona/templates/layout.js
+++ src/zona/templates/layout.js
@@ -58,6 +58,14 @@ function layout({ title, desc, canonical, ogImage, ogType = 'website', body, scr
 <link rel="icon" type="image/png" href="/assets/favicon.png">
 <link rel="apple-touch-icon" href="/assets/favicon.png">
 
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KH4VCQBZ');</script>
+<!-- End Google Tag Manager -->
+
 <!-- GA4 + Meta Pixel diferidos para no bloquear rendering -->
 <script>window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments)}
 setTimeout(function(){var s=document.createElement('script');s.async=true;s.src='https://www.googletagmanager.com/gtag/js?id=${ga4Id}';document.head.appendChild(s);s.onload=function(){gtag('js',new Date());gtag('config','${ga4Id}');}},2500);
@@ -406,6 +414,10 @@ footer{background:#050A14;padding:56px 6% 26px;border-top:1px solid var(--gl)}
 </style>
 </head>
 <body>
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KH4VCQBZ"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 <nav>
   <div class="nav-inner">
     <a href="/" class="logo"><img src="https://ik.imagekit.io/Zona/logo.png" alt="Zona INNmueble" style="height:52px;width:auto"></a>
